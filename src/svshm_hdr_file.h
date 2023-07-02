dbe9de85311403ca16e94a190c4fd368a114ee27
fox <664088852@qq.com>
2023-07-02 20:37:54 +0800

    添加修改注释

--- src/svshm_hdr_file.h
+++ src/svshm_hdr_file.h
@@ -5,28 +5,23 @@
 #include <sys/stat.h>
 #include <sys/sem.h>
 #include <sys/shm.h>
-#include "binary_sems.h"        /* Declares our binary semaphore functions */
+#include "binary_sems.h"
 #include "myhdr.h"
 
-/* Hard-coded keys for IPC objects */
-
-#define SHM_KEY 0x1234          /* Key for shared memory segment */
-#define SEM_KEY 0x5678          /* Key for semaphore set */
+#define SHM_KEY 0x1234          /* 共享内存的key */
+#define SEM_KEY 0x5678          /* 信号量集的key */
 
 #define OBJ_PERMS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP)
-                                /* Permissions for our IPC objects */
 
-/* Two semaphores are used to ensure exclusive, alternating access
-   to the shared memory segment */
 
-#define WRITE_SEM 0             /* Writer has access to shared memory */
-#define READ_SEM 1              /* Reader has access to shared memory */
+#define WRITE_SEM 0             /* 对共享内存写操作使用的信号量 */
+#define READ_SEM 1              /* 读取共享内存使用的信号量 */
 
-#ifndef BUF_SIZE                /* Allow "cc -D" to override definition */
-#define BUF_SIZE 1024           /* Size of transfer buffer */
+#ifndef BUF_SIZE
+#define BUF_SIZE 1024
 #endif
 
-struct shmseg {                 /* Defines structure of shared memory segment */
-    int cnt;                    /* Number of bytes used in 'buf' */
-    char buf[BUF_SIZE];         /* Data being transferred */
+struct shmseg {                 /* 使用共享内存通信使用的自定义数据结构 */
+    int cnt;                    /* buf中的字节数 */
+    char buf[BUF_SIZE];         /* 将发送的数据保存在这里 */
 };
